754fd61880d35872adc0603926e0200892feed01
nashvador <nashvador@gmail.com>
2022-07-15 16:06:19 -0400

    compile async await function and fix no response

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,16 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -12,18 +19,15 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = parseInt(process.env.PORT || "3001");
 app.get("/", (req, res) => {
-  axios_1.default
-    .get(
-      `https://api.coingecko.com/api/v3/ping
-  `
-    )
-    .then((response) => {
-      const data = response.data.gecko_says;
-      console.log(data);
-      res.send(data);
-    })
-    .catch((err) => console.log(err));
+    const sendGetRequest = () => __awaiter(void 0, void 0, void 0, function* () {
+        const apiResp = yield axios_1.default.get(`https://api.coingecko.com/api/v3/ping`);
+        const data = apiResp.data.gecko_says;
+        console.log(data);
+        res.send(data);
+        return data;
+    });
+    sendGetRequest();
 });
 app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
+    console.log(`[server]: Server is running at https://localhost:${port}`);
 });
--- index.ts
+++ index.ts
@@ -12,8 +12,10 @@ app.get("/", (req: Request, res: Response) => {
     const apiResp = await axios.get(`https://api.coingecko.com/api/v3/ping`);
     const data: object = apiResp.data.gecko_says;
     console.log(data);
+    res.send(data);
     return data;
   };
+  sendGetRequest();
 });
 
 app.listen(port, () => {
