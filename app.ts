f67801a1d5bf9dcccb32877d6a6d8d4f6803d30b
nashvador <nashvador@gmail.com>
2022-09-06 20:50:14 -0400

    Move everything to app.ts

---
+++ app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import portfolioRouter from "./routes/portfolio";
+import userRouter from "./routes/userRoutes";
+import loginRouter from "./routes/login";
+import apiRouter from "./routes/apiInfoRoutes";
+import { MONGODB_URI } from "./utils/config";
+const middleware = require("./utils/middleware");
+const cors = require("cors");
+import mongoose from "mongoose";
+const app = express();
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to Mongo");
+  })
+  .catch((error) => {
+    console.log("error connecting to Mongo", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
+app.use(middleware.userExtractor);
+
+app.use("/api/coininfo", apiRouter);
+app.use("/api/portfolio", portfolioRouter);
+app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
--- index.ts
+++ index.ts
@@ -1,38 +1,10 @@
-import express from "express";
-import portfolioRouter from "./routes/portfolio";
-import userRouter from "./routes/userRoutes";
-import loginRouter from "./routes/login";
-import apiRouter from "./routes/apiInfoRoutes";
-import { PORT, MONGODB_URI } from "./utils/config";
-const middleware = require("./utils/middleware");
-const cors = require("cors");
-import mongoose from "mongoose";
-const app = express();
+const app = require("./app"); // the actual Express application
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-mongoose
-  .connect(MONGODB_URI)
-  .then(() => {
-    console.log("connected to Mongo");
-  })
-  .catch((error) => {
-    console.log("error connecting to Mongo", error.message);
-  });
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-
-app.use(middleware.requestLogger);
-app.use(middleware.tokenExtractor);
-app.use(middleware.userExtractor);
-
-app.use("/api/coininfo", apiRouter);
-app.use("/api/portfolio", portfolioRouter);
-app.use("/api/users", userRouter);
-app.use("/api/login", loginRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
