7f2cb19bfc3bf030d3d6878ec4e70e1452c55c97
nashvador <nashvador@gmail.com>
2022-09-07 11:53:26 -0400

    Add api testing to the server

---
+++ tests/portfolio_api.test.ts
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+import server from "../index";
+const api = supertest(server);
+
+test("server can be pinged from Backend", async () => {
+  const apiURL: object = { url: "https://api.coingecko.com/api/v3/ping" };
+  const response = await api.post("/api/coininfo").send(apiURL);
+  expect(response.type).toEqual("application/json");
+  expect(response.body.gecko_says).toEqual("(V3) To the Moon!");
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
--- utils/config.ts
+++ utils/config.ts
@@ -1,3 +1,7 @@
 require("dotenv").config();
 export const PORT = process.env.PORT;
-export const MONGODB_URI: string = process.env.MONGODB_URI!;
+
+export const MONGODB_URI: string =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI!
+    : process.env.MONGODB_URI!;
