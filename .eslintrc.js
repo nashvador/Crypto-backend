bb5ed68cf6dab9e1ec20da3245fb4c215a8dec88
nashvador <nashvador@gmail.com>
2022-09-23 09:56:02 -0400

    Add Linting to the packagejson

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/semi": [1],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/restrict-plus-operands": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unused-vars": [1, { argsIgnorePattern: "^_" }],
+    "no-case-declarations": "off",
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/ban-types": 0,
+  },
+};
