1cbea757b17dbab92ebd0ca89596624218b3ff08
nashvador <nashvador@gmail.com>
2022-09-26 14:43:31 -0400

    Heroku should work

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-const app = require("./app.ts"); // the actual Express application
+const app = require("./app"); // the actual Express application
 const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
